77185446ddc162145dee204abc0c37a11e73fc68
Chantee Hoarau <chantee.hoarau123@gmail.com>
2023-08-10 22:16:42 +0200

    Add an append and prepend function

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ class LinkedList{
     constructor(head = null){
         this.head = head;
     }
+
+    append(value){
+        let newNode = new Node(value);
+        if (this.head === null){
+            this.head = newNode;
+            return this.head;
+        }
+         let current = this.head;
+        while(current.next){
+            current = current.next;
+        }
+            current.next = newNode;
+            return this.head;
+    }
+    
+    prepend(value){
+        let newNode = new Node(value);
+        if(this.head === null){
+            this.head = newNode;
+            return this.head
+        }else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        return this.head;
+    }
 }
 
 class Node{
